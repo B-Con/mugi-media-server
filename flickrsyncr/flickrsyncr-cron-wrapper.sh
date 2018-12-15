fd104dbd76f168af7a5e63e220ac43f2de56b067
Brad Conte <brad@bradconte.com>
2018-12-15 02:12:22 -0800

    Change server's config root to /config.

--- flickrsyncr/flickrsyncr-cron-wrapper.sh
+++ flickrsyncr/flickrsyncr-cron-wrapper.sh
@@ -3,7 +3,7 @@
 # Execute the flickrsyncr command to sync local to the cloud.
 # * Uses the bin/flickrsyncr installed with pip install flickrsyncr.
 # * Don't set --logfile to use stderr, easier integration with docker.
-docker-compose -f /home/b-con/mugi-server/docker-compose.yml run --detach flickrsyncr \
+docker-compose -f /config/mugi-server/docker-compose.yml run --detach flickrsyncr \
 	--config_dir /config --path /data/wallpapers/backdrop \
 	--album Backdrop --push --sync --checksum --loglevel WARNING --dryrun
 
--- setup.sh
+++ setup.sh
@@ -1,5 +1,40 @@
 #!/bin/sh
 
-groupadd -g 1000 b-con
-useradd -u 1000 -g 1000 -G sudo b-con
+S_USER=b-con
+S_UID=1000
+S_GID=1000
+
+cd /config/mugi-server
+
+# Install necessary pacakges.
+apt-get install ...
+
+# Create the main user and a group named after them if they do not already
+# exist. Add dotfiles and SSH key login.
+getent group $S_USER || groupadd -g $S_GID $S_USER
+getent passwd $S_USER || useradd -u $S_UID -g $G_UID -G sudo $S_USER
+sudo -u $S_USER /bin/sh <<EOF
+	cd ~
+	git clone https://github.com/B-Con/dotfiles.git
+	cd .ssh
+	cat id_mugi.pub >> authorized_keys
+	chmod 600 authorized_keys
+EOF
+
+# Install custom system settings.
+# ./etc is a flat collection of /etc-esque files, NOT a skeleton /etc.
+install etc/sshd_config /etc/ssh/sshd_config
+install etc/exports     /etc/exports
+
+# Restart services to pickup new settings.
+systemctl restart sshd
+systemctl restart nfs-server
+
+# Install the crons.
+crontab -u $S_USER etc/crontab.b-con
+crontab -u root    etc/crontab.root
+
+# Start the services.
+docker-compose up deluge
+docker-compose up plex
 
