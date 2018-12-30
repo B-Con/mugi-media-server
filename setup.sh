8bedd8a841567d46ff1ac0b20360ed434ede95fd
Brad Conte <brad@bradconte.com>
2018-12-30 02:55:43 -0800

    Lots os misc setup.sh changes. Patch in commands used to setup live system. Fix typos. Re-org a bit.

--- setup.sh
+++ setup.sh
@@ -4,21 +4,36 @@ S_USER=b-con
 S_UID=1000
 S_GID=1000
 
-# Install necessary core pacakges.
-apt-get install git sudo cron vim apt-transport-https traceroute tar gunzip smartmontools wget curl dnsutils ntp
+#####
+# TODO:
+# * Configure caddy certs.
+#####
 
-# Install Docker.
-echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
-# Install ZFS from contrib, see https://github.com/zfsonlinux/zfs/wiki/Debian
+# Install core packages.
+apt-get install git sudo cron vim apt-transport-https tar gunzip smartmontools 
+apt-get install traceroute wget curl dnsutils ntp
+
+# Enable contrib repo (necessary for ZFS).
 echo "Edit /etc/apt/sources.list and add 'contrib'."
 read CONT
 vim /etc/apt/sources.list
-
 apt-get update
+
+# Install server-specific functionality.
+# ZFS docs: https://github.com/zfsonlinux/zfs/wiki/Debian
 apt-get install spl-dkms linux-headers-$(uname -r)
-apt-get install zfs-dkms zfsutils-linux zfsnap
-apt-get install docker-ce docker-compose
 apt-get install nfs-common nfs-kernel-server
+apt-get install zfs-dkms zfsutils-linux zfsnap
+
+# Install Docker.
+# docker - community edition from docker repos
+# jq - json parser (for docker outputs)
+# docker-compose - direct pull because there's no package for some insane reason.
+echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee -a /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install docker-ce jq
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)
+" -o /usr/local/bin/docker-compose
 
 # Create the main user and a group named after them if they do not already
 # exist. Add dotfiles and SSH key login. Stomp whatever is already there.
@@ -31,27 +46,26 @@ sudo -u $S_USER /bin/sh <<EOF
 	cat id_mugi.pub >> authorized_keys
 	chmod 600 authorized_keys
 EOF
-rm -rf /home/b-con
-mv /tmp/dotfiles /home/b-con
+rm -rf /home/$S_USER
+mv /tmp/dotfiles /home/$S_USER
 
 # Setup the server's config root.
 mkdir -p /config/mugi-server
 chown root:root /config
 chmod 755 /config
-chown b-con:b-con /config/mugi-server
+chown $S_USER:$S_USER /config/mugi-server
 chmod 755 /config/mugi-server
 cd /config
-sudo -u b-con git clone https://github.con/B-Con/mugi-server.git
+sudo -u $S_USER git clone https://github.con/B-Con/mugi-server.git
 
 # Restore ZFS pool "pot", with volume "tea" into /media/tea.
 mkdir -p /media/pot
 mkdir -p /media/tea
-chown b-con:b-con /media/tea
+chown $S_USER:$S_USER /media/tea
 zpool import pot
 
 # Install custom system settings, reload the configs.
 # ./etc is a flat collection of /etc-esque files, NOT a skeleton /etc.
-
 install etc/sshd_config /etc/ssh/sshd_config
 install etc/exports     /etc/exports
 systemctl restart sshd
@@ -63,7 +77,7 @@ crontab -u $S_USER etc/crontab.b-con
 crontab -u root    etc/crontab.root
 
 # Allow manual steps (eg, restoring caches) before starting services.
-echo "Everything's installed. Start services...? [y/n]"
+echo "Everything's installed. Start docker services...? [y/n]"
 read CONT
 [ "$CONT" = y ] || exit 0
 
@@ -71,4 +85,4 @@ read CONT
 docker-compose up deluge
 docker-compose up plex
 docker-compose up caddy
-# TODO: Configure caddy certs (and other?)
+
